64c32f691225aa8107bcfc72833f0c45ec97a1fe
avichai321 <77723644+avichai321@users.noreply.github.com>
2022-06-09 22:53:07 +0300

    done

---
+++ hw_routes_api_16_5_22/Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hw_routes_api_16_5_22.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [Route("{id:int:MinLength(5)}")]
+        public string GetId(int id)
+        {
+            return "my id need to be 5 numbers " + id;
+        }
+        
+        [Route("{book:regex(zoom|boom)}")]
+        public string GetBook(string book)
+        {
+            return "this book is the " + book;
+        }
+    }
+}
